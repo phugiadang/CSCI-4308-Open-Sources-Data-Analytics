851f07f7a4367c6f8486d3caeda3ed3707f84d47
root <root@open-source-data-analytics-1.novalocal>
2016-02-14 15:32:01 -0700

    Updated candidate tweet count

--- OpenStack/getGDELT.py
+++ OpenStack/getGDELT.py
@@ -1,7 +1,7 @@
 import sys
 from subprocess import call
 
-a = call("sudo sh -c \"date | cut -d ' ' -f 2-5 > date.txt\"", shell=True)
+a = call("sudo sh -c \"date | cut -d ' ' -f 2-6 > date.txt\"", shell=True)
 
 
 date_dictionary = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May" : "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12"}
