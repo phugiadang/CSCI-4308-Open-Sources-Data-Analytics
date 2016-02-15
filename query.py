87e4a40a0282ae6010c92611acf5a52ee00c7211
root <root@open-source-data-analytics-1.novalocal>
2016-02-14 17:04:53 -0700

    Updated query.py

--- query.py
+++ query.py
@@ -1,6 +1,7 @@
 from sys import argv
 from cassandra.cluster import Cluster
 from subprocess import call
+import operator
 
 keyspace = 'candidates'
 cluster = Cluster(
@@ -38,15 +39,35 @@ if (quantity == 'hourly'):
             new_start_date = start_date
             new_end_date = str(int(start_date) + 10000)
 
+        
+        #junk = sorted(list_of_counts["bush"].items(), key=operator.itemgetter(0))
+        #print junk
         for i in range (0, len(list_of_candidates)):
-            print list_of_candidates[i] + ": " + str(list_of_counts[list_of_candidates[i]]) + "\n"
+            junk = sorted(list_of_counts[list_of_candidates[i]].items(), key=operator.itemgetter(0))
+
+            print list_of_candidates[i] + ": " + str(junk) + "\n\n"
 
     else:
-        final_step = session.execute('select count(*) from ' + candidate + ' where created_at >= ' + start_date + ' and created_at <= ' + end_date + ' ALLOW FILTERING;')
+        list_of_dates = {}
+        new_start_date = start_date
+        new_end_date = str(int(new_start_date) + 10000)
 
-        result_string = final_step[0]
+        while(int(new_start_date) != int(end_date)):
+                result = session.execute('select count(*) from ' + candidate + ' where created_at >= ' + new_start_date + ' and created_at < ' + new_end_date + ' ALLOW FILTERING;')
 
-        print result_string[0]
+                list_of_dates['from ' + new_start_date + ' to ' + new_end_date] = result[0][0]
+                new_start_date = new_end_date
+                new_end_date = str(int(new_start_date) + 10000)
+                if ((new_end_date[8] == '2') and (new_end_date[9] == '4')):
+                    new_end_date = str(int(new_end_date) + 760000)
+
+
+
+  
+        #final_step = session.execute('select count(*) from ' + candidate + ' where created_at >= ' + start_date + ' and created_at <= ' + end_date + ' ALLOW FILTERING;')
+
+        print candidate + str(sorted(list_of_dates.items(), key=operator.itemgetter(0)))
+        #print result_string[0]
 
 
 
