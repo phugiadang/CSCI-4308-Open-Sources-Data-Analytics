d176f2c556b207ef110aaed70a4d3a1436fa1882
root <root@open-source-data-analytics-1.novalocal>
2016-02-15 23:48:01 -0700

    Updated candidate tweet count

--- OpenStack/finalize.py
+++ OpenStack/finalize.py
@@ -2,7 +2,7 @@ last = open("finalNumbers.md", 'w+')
 
 pos_dict = {1 : "First", 2 : "Second", 3 : "Third", 4 : "Fourth", 5 : "Fifth", 6: "Sixth", 7 : "Seventh", 8 : "Last"}
 
-with open("tweetCount.md", "r+") as number_file:
+with open("tempFiles.txt", "r+") as number_file:
     line_number = 0
     for line in number_file:
         if (line_number == 0):
