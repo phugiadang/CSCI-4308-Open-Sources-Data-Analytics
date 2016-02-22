ad86830511ed13d1cfa5915dc76106705f7e4408
root <root@open-source-data-analytics-1.novalocal>
2016-02-21 17:57:01 -0700

    Updated candidate tweet count

--- OpenStack/commas.py
+++ OpenStack/commas.py
@@ -1,16 +1,17 @@
 import sys
 
-a = sys.arg[1]
-b=''
-for x in range(1,len(a)+1):
-    if (x %3 == 0):
-        if (x != len(a)):
-            b = ','+a[len(a)-x]+b
-        else:
-            b = a[len(a)-x]+b
-    else:
-        b = a[len(a)-x]+b
+def commify():
+	a = sys.arg[1]
+	b=''
+	for x in range(1,len(a)+1):
+	    if (x %3 == 0):
+	        if (x != len(a)):
+	            b = ','+a[len(a)-x]+b
+	        else:
+	            b = a[len(a)-x]+b
+	    else:
+       	 b = a[len(a)-x]+b
 
 
 
-print b
+
