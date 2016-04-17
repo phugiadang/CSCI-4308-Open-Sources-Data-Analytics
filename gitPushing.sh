9e6bb0b40f928dcfd3df783fd2cb3db02424cb83
CommandoScorch16 <root@open-source-data-analytics-1.novalocal>
2016-04-17 04:20:28 +0000

    Daily commit

--- gitPushing.sh
+++ gitPushing.sh
@@ -1,10 +1,6 @@
 sudo git pull
 
-<<<<<<< HEAD
 sudo git add . --a
 sudo git status
-=======
-sudo git add . --all
->>>>>>> c5e7f8abe93536347055196eaed35b435e95b873
 sudo git commit -m "Daily commit"
 sudo git push origin master
